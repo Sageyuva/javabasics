bd35000d793430b47cd77b9d00f2a39406d78ea3
Sageyuva <yuvarajdhammure@gmail.com>
2022-09-12 22:13:10 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+import javax.swing.*;
+import java.sql.SQLOutput;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+  val read= Scanner(System.'in')
+    int num1 = read.nextInt();
+    }
+}
