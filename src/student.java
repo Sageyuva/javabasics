8bc185b1a3e8c8ad0c82c1044a18c341a1d0f791
Sageyuva <yuvarajdhammure@gmail.com>
2022-09-22 10:31:59 +0530

    completed

--- src/student.java
+++ src/student.java
@@ -1,25 +1,22 @@
-import java.awt.*;
+class A{
+    private int value;
 
-public class student {
-
-    private student(String name,String middlename,String surname,int usn,int number,int batch){
-
-        System.out.println("Name of the student " +name);
-        System.out.println("Father name of the  student is " +middlename);
-        System.out.println("surame of the student " +surname);
-        System.out.println("USN of the student "+usn);
-        System.out.println("Phone number of the student "+ number);
-        System.out.println("Batch of the student is  "+batch);
-    }
-
-
-}
-class b{
-    public static void main(String[] args) {
-        student no1 = new
-                student("Yuvaraj","Hanamanthrao","Dhammure",158,99999,19);
-        student no2=new student("Nikhil","Hanamanthrao","Jawali",83,9999090,19);
-        student no3=new student("Vinay","Subhash","Nirwane",153,99999,19);
+    public void setValue(int m){
+        value=m;
 
     }
+    public int getValue(){
+        return value;
+    }
+
 }
+ class b{
+     public static void main(String[] args) {
+         A m = new A();
+         m.setValue(100);
+         System.out.println(m.getValue());
+     }
+
+
+
+ }
